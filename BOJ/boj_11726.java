d83debe6230113b90cda73f0d8ba03069b84a76a
devpcjin <sgn03077@gmail.com>
2022-05-28 15:32:38 +0900

    [BOJ] 11726. 2xn 타일링

---
+++ BOJ/boj_11726.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_11726 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static int[] dp = new int[10001];
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i-1]+dp[i-2])%10007;
+        }
+
+        System.out.println(dp[n]%10007);
+    }
+
+}
