673e42c8e1a5258e4fc06815071cc2b99084bbd3
devpcjin <sgn03077@gmail.com>
2022-05-28 14:13:42 +0900

    [BOJ] 9095. 1,2,3 더하기

---
+++ BOJ/boj_9095.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class boj_9095 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static Scanner sc = new Scanner(System.in);
+    public static int t;
+
+    public static int[] dp = new int[11];
+
+
+    public static void main(String[] args) throws IOException {
+        t = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= t; i++) {
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            int n = Integer.parseInt(br.readLine());
+
+            for (int j = 4; j <= n; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+            }
+            sb.append(dp[n]).append("\n");
+
+        }
+
+        System.out.println(sb);
+    }
+
+
+}
