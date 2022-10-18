cf16e8fa0968c3f2426c20009bae144a8aa5d692
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2022-10-18 17:01:55 +0900

    [Programmers[ 120853. 컨트롤 제트

---
+++ Programmers/Lv. 0/prog_120853.java
@@ -0,0 +1,28 @@
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+class Solution {
+    
+    public int solution(String s) {
+        int answer = 0;
+        
+        StringTokenizer st = new StringTokenizer(s);
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        
+        while(st.hasMoreTokens()) {
+            String a = st.nextToken();
+            if(a.equals("Z")) {
+                arrayList.remove(arrayList.size()-1);
+            }else {
+                arrayList.add(Integer.parseInt(a));
+            }
+        }
+        
+        for(int i = 0; i < arrayList.size(); i++) {
+            answer += arrayList.get(i);
+        }
+        
+        return answer;
+    }
+    
+}
