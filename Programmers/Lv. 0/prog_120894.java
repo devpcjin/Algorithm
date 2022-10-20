dccf82bf97ba0cbdea88e736bf2d9311ddc68d10
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2022-10-20 11:38:11 +0900

    [Programmers] 120894. 영어가 싫어요 + Refactoring1

--- Programmers/Lv. 0/prog_120894.java
+++ Programmers/Lv. 0/prog_120894.java
@@ -3,55 +3,18 @@ class Solution {
         long answer = 0;
         int i = 0;
         
-        StringBuilder sb = new StringBuilder();
+        numbers = numbers.replaceAll("zero", "0");
+        numbers = numbers.replaceAll("one", "1");
+        numbers = numbers.replaceAll("two", "2");
+        numbers = numbers.replaceAll("three", "3");
+        numbers = numbers.replaceAll("four", "4");
+        numbers = numbers.replaceAll("five", "5");
+        numbers = numbers.replaceAll("six", "6");
+        numbers = numbers.replaceAll("seven", "7");
+        numbers = numbers.replaceAll("eight", "8");
+        numbers = numbers.replaceAll("nine", "9");
         
-        while(i <= numbers.length()-2) {
-            if(numbers.charAt(i) == 'z') {
-                i += 4;
-                sb.append('0');
-            } else if(numbers.charAt(i) == 'o') {
-                i += 3;
-                sb.append('1');
-                
-            } else if(numbers.charAt(i) == 't') {
-                if(numbers.charAt(i+1) == 'w') {
-                    i += 3;
-                    sb.append('2');
-                } else {
-                    i += 5;
-                    sb.append('3');
-                }
-                
-            } else if(numbers.charAt(i) == 'f') {
-                if(numbers.charAt(i+1) == 'o') {
-                    i += 4;
-                    sb.append('4');
-                } else {
-                    i += 4;
-                    sb.append('5');
-                }
-                
-            } else if(numbers.charAt(i) == 's') {
-                if(numbers.charAt(i+1) == 'i') {
-                    i += 3;
-                    sb.append('6');
-                } else {
-                    i += 5;
-                    sb.append('7');
-                }
-                
-            } else if(numbers.charAt(i) == 'e') {
-                i += 5;
-                sb.append('8');     
-            } else if(numbers.charAt(i) == 'n') {
-                i += 4;
-                sb.append('9');
-            }
-        }
-        
-        String s = sb.toString();
-        
-        answer = Long.parseLong(s);
+        answer = Long.parseLong(numbers);
         
         
         return answer;
