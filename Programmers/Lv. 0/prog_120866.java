ebd831a6300c18ebd74511d7752340bf8b674fbb
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2022-10-24 15:15:46 +0900

    [Programmers] 120866. 안전지대

--- Programmers/Lv. 0/prog_120866.java
+++ Programmers/Lv. 0/prog_120866.java
@@ -1,84 +1,30 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[][] board) {
         int answer = 0;
         
-        boolean bool[][] = new boolean[board.length][board.length];
+        boolean bool[][] = new boolean[board.length+2][board.length+2];
         
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board.length; j++) {
                 if(board[i][j] == 1) {
-                    if(i == 0 && j == 0) {
-                        bool[i][j] = true;
-                        bool[i+1][j] = true;
-                        bool[i][j+1] = true;
-                        bool[i+1][j+1] = true;
-                    } else if (i == 0 && j == board.length-1) {
-                        bool[i][j] = true;
-                        bool[i+1][j] = true;
-                        bool[i][j-1] = true;
-                        bool[i+1][j-1] = true;
-                    } else if (i == board.length-1 && j == 0) {
-                        bool[i][j] = true;
-                        bool[i-1][j] = true;
-                        bool[i][j+1] = true;
-                        bool[i-1][j+1] = true;
-                    } else if (i == board.length-1 && j == board.length-1) {
-                        bool[i][j] = true;
-                        bool[i-1][j] = true;
-                        bool[i][j-1] = true;
-                        bool[i-1][j-1] = true;
-                    } else if(i == 0 && (j != 0 || j != board.length-1)) {
-                        bool[i][j] = true;
-                        bool[i][j-1] = true;
-                        bool[i+1][j-1] = true;
-                        bool[i+1][j] = true;
-                        bool[i][j+1] = true;
-                        bool[i+1][j+1] = true;
-                    } else if(i == board.length-1 && (j != 0 || j != board.length-1)) {
-                        bool[i][j] = true;
-                        bool[i-1][j] = true;
-                        bool[i][j-1] = true;
-                        bool[i-1][j-1] = true;
-                        bool[i][j+1] = true;
-                        bool[i-1][j+1] = true;
-                    } else if(j == board.length-1 && (i != 0 || i != board.length-1)) {
-                        bool[i][j] = true;
-                        bool[i][j-1] = true;
-                        bool[i+1][j] = true;
-                        bool[i+1][j-1] = true;
-                        bool[i-1][j] = true;
-                        bool[i-1][j-1] = true;
-                    } else if(j == 0 && (i != 0 || i != board.length-1)) {
-                        bool[i][j] = true;
-                        bool[i][j+1] = true;
-                        bool[i+1][j] = true;
-                        bool[i+1][j+1] = true;
-                        bool[i-1][j] = true;
-                        bool[i-1][j+1] = true;
-                    } else {
-                        bool[i-1][j-1] = true;
-                        bool[i][j-1] = true;
-                        bool[i+1][j-1] = true;
-                        bool[i-1][j] = true;
-                        bool[i][j] = true;
-                        bool[i+1][j] = true;
-                        bool[i-1][j+1] = true;
-                        bool[i][j+1] = true;
-                        bool[i+1][j+1] = true;
-                    }
+                    bool[i][j] = true;
+                    bool[i+1][j] = true;
+                    bool[i+2][j] = true;
+                    bool[i][j+1] = true;
+                    bool[i+1][j+1] = true;
+                    bool[i+2][j+1] = true;
+                    bool[i][j+2] = true;
+                    bool[i+1][j+2] = true;
+                    bool[i+2][j+2] = true;
                 }
             }
         }
         
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board.length; j++) {
-                
+        for(int i = 1; i < bool.length-1; i++) {
+            for(int j = 1; j < bool.length-1; j++) {
                 if(!bool[i][j]) {
                     answer++;
                 }
-                
             }
         }
         
