a1afec28e998414be4bbc0a106b069a2a96d0217
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2022-10-24 20:53:12 +0900

    [Programmers] 120878. 유한소수 판별하기

---
+++ Programmers/Lv. 0/prog_120878.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int a, int b) {
+        int denominator = b / gcd(a, b);
+
+        while (denominator != 1) {
+            if (denominator % 2 == 0) {
+                denominator /= 2;
+            } else if (denominator % 5 == 0) {
+                denominator /= 5;
+            } else {
+                return 2;
+            }
+        }
+
+        return 1;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
