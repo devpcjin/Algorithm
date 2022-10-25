c8ede1dd3613d1a60b3bf5ddc7de39ef6a8c88ea
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2022-10-25 16:59:10 +0900

    [Programmers] 120924. 다음에 올 숫자

---
+++ Programmers/Lv. 0/prog_120924.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        if((common[common.length-1] - common[common.length-2]) 
+           == (common[common.length-2] - common[common.length-3])) {
+            answer = common[common.length-1] + (common[common.length-1] - common[common.length-2]);
+        }else if ((common[common.length-1] / common[common.length-2]) 
+           == (common[common.length-2] / common[common.length-3])) {
+            answer = common[common.length-1] * (common[common.length-1] / common[common.length-2]);
+        }
+        
+        return answer;
+    }
+}
