4604209c31770a7a3e712253d253944020d5c644
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2022-10-26 21:09:58 +0900

    [Programmers] 120882. 등수 매기기 + Refactoring1

--- Programmers/Lv. 0/prog_120882.java
+++ Programmers/Lv. 0/prog_120882.java
@@ -1,42 +1,20 @@
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collections;
-
+import java.util.*;
 class Solution {
-    public int[] solution(int[][] score) {
+    
+    public int[] solution(int[][] score){
         int[] answer = new int[score.length];
-        int[] avg = new int[score.length];
+        float[] avg = new float[score.length];
         
+        for(int i = 0; i < score.length; i++)
+            avg[i] = (float)(score[i][0] + score[i][1])/2;
         
-        for(int i = 0; i < score.length; i++) {
-            avg[i] = (score[i][0] + score[i][1]) / 2;
-        }
-        
-        
-        //동일점수 중복 제거
-        ArrayList<Integer> avgDuplicate = new ArrayList<> ();
-        
-        for(int item : avg) {
-            if(!avgDuplicate.contains(item)) {
-                avgDuplicate.add(item);
+        for(int i = 0; i < score.length; i++){
+            answer[i] = 1;
+            for(int j = 0; j < avg.length; j++){
+                if(avg[i] < avg[j])
+                    answer[i]++;
             }
         }
-        Collections.sort(avgDuplicate, Collections.reverseOrder());
-        
-        int rank = 1;
-        int same = 0;
-        
-        for(int i = 0; i < avgDuplicate.size(); i++) {
-            same = 0;
-            for(int j = 0; j < avg.length; j++) {
-                if(avg[j] == avgDuplicate.get(i)) {
-                    answer[j] = rank;
-                    same++;
-                }
-            }
-            rank += same;
-        }
-        
         return answer;
     }
 }
