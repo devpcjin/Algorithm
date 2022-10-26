bed8f2d438a36dc620a6fd0429256328d7af73d2
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2022-10-27 03:20:30 +0900

    [Programmers] 120875. 평행 + refactoring1

--- Programmers/Lv. 0/prog_120875.java
+++ Programmers/Lv. 0/prog_120875.java
@@ -12,6 +12,14 @@ class Solution {
             for(int j = i+1; j < dots.length; j++) {
                 
                 double lean = (double)((double)dots[i][1] - (double)dots[j][1]) / ((double)dots[i][0]-(double)dots[j][0]);
+                
+                //문모 분자가 0일 때, 추가
+                if (((double)dots[i][0]-(double)dots[j][0]) == 0) {
+                    lean = 0;
+                } else if ((double)dots[i][1] - (double)dots[j][1] == 0) {
+                    lean = -999;
+                }
+                
                 if(check.contains(lean)) {
                     return 1;
                 }
