148b90a30d1f0ebef8630a356df5df4453adc8bc
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2023-06-13 14:40:04 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.Map.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < clothes.length; i++) {
+            if(map.containsKey(clothes[i][1])) {
+                map.put(clothes[i][1], map.get(clothes[i][1])+1);
+            } else {
+                map.put(clothes[i][1], 1);
+            }
+        }
+        for(Entry<String, Integer> entrySet: map.entrySet()){
+            answer *= (entrySet.getValue()+1);
+        }
+        
+        return answer-1;
+    }
+}
