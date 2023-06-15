4667854ba6665dd7cd705123a5ea3ef5bdff7f91
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2023-06-15 09:11:43 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.17 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        int totalWeight = 0;
+        int time = 0;
+        int passedTruck = 0;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int truck : truck_weights) {
+            list.add(truck);
+        }
+        
+        while(passedTruck < truck_weights.length) {
+            if(!list.isEmpty()){
+                if(queue.isEmpty()) {
+                    queue.add(list.get(0));
+                    totalWeight += list.get(0);
+                    list.remove(0);
+                    time++;
+                } else if(queue.size() == bridge_length) {
+                    int out = queue.poll();
+                    totalWeight -= out;
+                    if(out != 0) {
+                        passedTruck++;
+                    }
+                } else {
+                    if(totalWeight + list.get(0) <= weight) {
+                        queue.add(list.get(0));
+                        totalWeight += list.get(0);
+                        list.remove(0);
+                        time++;
+                    } else {
+                        queue.add(0);
+                        time++;
+                    }
+                }
+            } else {
+                if(queue.size() == bridge_length) {
+                    if(queue.poll() != 0) {
+                        passedTruck++;
+                    }
+                } else {
+                    queue.add(0);
+                    time++;
+                }
+            }
+        }
+        
+        answer = time + 1;
+        
+        return answer;
+        
+    }
+}
