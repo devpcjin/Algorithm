c5bfe760c77d75ab672965bd79ec96336ade4b8f
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2023-06-15 09:38:18 +0900

    [level 2] Title: 주식가격, Time: 12.59 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0; i < prices.length; i++) {
+            int time = 0;
+            for(int j = i+1; j < prices.length; j++) {
+                if(prices[i] <= prices[j]) time++;
+                else {
+                    time++;
+                    break;
+                }
+            }
+            answer[i] = time;
+        }
+        
+        return answer;
+    }
+}
