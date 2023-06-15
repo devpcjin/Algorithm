86d5e5d1a65403736fc39fb8d8124b9c969509f4
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2023-06-16 08:57:18 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -2,19 +2,48 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        
-        for(int i = 0; i < commands.length; i++) {
-            int[] splitArray = new int[commands[i][1]-commands[i][0] + 1];
-            int index = 0;
-            for(int j = commands[i][0]-1; j < commands[i][1]; j++) {
-                splitArray[index] = array[j];
-                index++;
+        int n = 0;
+        int[] ret = new int[commands.length];
+
+        while(n < commands.length){
+            int m = commands[n][1] - commands[n][0] + 1;
+
+            if(m == 1){
+                ret[n] = array[commands[n++][0]-1];
+                continue;
             }
-            Arrays.sort(splitArray);
-            answer[i] = splitArray[commands[i][2] - 1];
+
+            int[] a = new int[m];
+            int j = 0;
+            for(int i = commands[n][0]-1; i < commands[n][1]; i++)
+                a[j++] = array[i];
+
+            sort(a, 0, m-1);
+
+            ret[n] = a[commands[n++][2]-1];
         }
-        
-        return answer;
+
+        return ret;
+    }
+
+    void sort(int[] a, int left, int right){
+        int pl = left;
+        int pr = right;
+        int x = a[(pl+pr)/2];
+
+        do{
+            while(a[pl] < x) pl++;
+            while(a[pr] > x) pr--;
+            if(pl <= pr){
+                int temp = a[pl];
+                a[pl] = a[pr];
+                a[pr] = temp;
+                pl++;
+                pr--;
+            }
+        }while(pl <= pr);
+
+        if(left < pr) sort(a, left, pr);
+        if(right > pl) sort(a, pl, right);
     }
 }
