6813b9fb01103adaf670ebc4a3e1aa15829ce5b6
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-02 15:20:24 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 22.70 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(my_string: String, n: Int): String {
+        var answer: String = ""
+        
+        answer = my_string.slice(0 until n).toString()
+        return answer
+    }
+}
