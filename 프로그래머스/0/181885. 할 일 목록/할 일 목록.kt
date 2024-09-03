faf412b18b9fcd2059bc014de36d62405faded9b
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-03 14:45:58 +0900

    [level 0] Title: 할 일 목록, Time: 0.08 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
@@ -1,4 +1,4 @@
 class Solution {
     fun solution(todo_list: Array<String>, finished: BooleanArray): Array<String>
-    = todo_list.mapIndexed{ i, v -> if(!finished[i]) v else "" }.filter{ it != ""}.toTypedArray()
+    = todo_list.filterIndexed{ i, _ -> !finished[i] }.toTypedArray()
 }
