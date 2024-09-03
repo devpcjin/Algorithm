99f8c789e108e1648ef3949ded0c2910be4c4c86
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-03 13:52:14 +0900

    [level 0] Title: x 사이의 개수, Time: 26.52 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.kt
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.kt
@@ -1,18 +1,3 @@
 class Solution {
-    fun solution(myString: String): IntArray {
-        var answer: IntArray = intArrayOf()
-        var count: Int = 0;
-        
-        myString.forEach {
-            if(it == 'x') {
-                answer += count;
-                count = 0;
-            } else {
-                count++
-            }
-        }
-        answer += count;
-        
-        return answer
-    }
+    fun solution(myString: String): IntArray = myString.split("x").map { it.length }.toIntArray()
 }
