7a5b8a36d226775cd117441d99809e9214caa11a
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-03 12:31:54 +0900

    [level 0] Title: A 강조하기, Time: 11.93 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.kt
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.kt
@@ -2,11 +2,7 @@ class Solution {
     fun solution(myString: String): String {
         var answer: String = ""
         
-        answer = myString.map { 
-            if(it.code == 65 || it.code == 97) 'A' 
-            else if(it.code < 97 && it.code != 32) (it.code+32).toChar() 
-            else it
-        }.joinToString("")
+        answer = myString.toLowerCase().replace("a","A")
         return answer
     }
 }
