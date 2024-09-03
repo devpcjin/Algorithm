2b9f301577d18978ffeaf2427e317634905c2299
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-03 14:34:30 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.42 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.kt
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.kt
@@ -1,18 +1,6 @@
 class Solution {
     fun solution(n: Int): Array<IntArray> {
-        var answer: Array<IntArray> = arrayOf<IntArray>()
-        var innerArray: IntArray = intArrayOf()
         
-        for(i in 0 until n) {
-            for( j in 0 until n) {
-                if(i == j) innerArray += 1
-                else innerArray += 0
-            }
-            answer+=innerArray
-            innerArray = intArrayOf()
-        }
-        
-        
-        return answer
+        return Array(n) {i -> IntArray(n) { j -> if(i==j) 1 else 0}}
     }
 }
