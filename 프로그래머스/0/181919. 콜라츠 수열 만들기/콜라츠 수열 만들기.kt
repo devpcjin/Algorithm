1ca8499c182377e48316f62581dda18acba0b090
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-03 10:57:39 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 11.79 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.kt
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.kt
@@ -4,7 +4,7 @@ class Solution {
         var x: Int = n
         
         while(x != 1) {
-            if(x%2==0) x = x/2
+            if(x%2==0) x /= 2
             else x = 3*x+1
             
             answer += x
