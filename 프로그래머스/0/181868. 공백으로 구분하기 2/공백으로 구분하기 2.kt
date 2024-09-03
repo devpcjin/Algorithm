1056a894630fd9392c744a8a33fa80dfed521ef6
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-03 11:27:13 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 15.75 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
@@ -2,19 +2,7 @@ class Solution {
     fun solution(my_string: String): Array<String> {
         var answer: Array<String> = arrayOf<String>()
         
-        var tempString = ""
-        
-        for(i in my_string) {
-            print(i)
-            if(i.code != 32)  tempString += i
-            else {
-                if(tempString !== "") {
-                    answer += tempString
-                    tempString = ""
-                }
-            }
-        }
-        if(tempString != "") answer += tempString
+        answer = my_string.split(" ").filter { it.isNotBlank() }.toTypedArray()
         return answer
     }
 }
