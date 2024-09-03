cfd149212de73bb56888c4b31fc7f884d14c504c
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-03 09:44:52 +0900

    [level 0] Title: 0 떼기, Time: 30.08 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.kt
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.kt
@@ -1,16 +1,5 @@
 class Solution {
     fun solution(n_str: String): String {
-        var answer: String = ""
-        var idx: Int = 0
-        
-        for((i, v) in n_str.withIndex()) {
-            if(v != '0') {
-                idx = i
-                break;
-            }
-        }
-        
-        answer = n_str.slice(idx until n_str.length)
-        return answer
+        return n_str.trimStart('0')
     }
 }
