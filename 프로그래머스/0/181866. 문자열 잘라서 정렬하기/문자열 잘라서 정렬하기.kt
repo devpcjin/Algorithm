6fe59ac3eb4d56740f4fe7c0158d2230a6b6dca6
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-04 08:00:25 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 45.62 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(myString: String): Array<String> {
+        var answer: Array<String> = arrayOf<String>()
+    
+        return myString.split("x").filter { it.isNotEmpty() }.sorted().toTypedArray()
+    }
+}
