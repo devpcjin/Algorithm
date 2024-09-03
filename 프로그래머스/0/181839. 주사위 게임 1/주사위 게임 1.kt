b85f87e708fef01c03f033d277a8dee4cffac26b
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-03 10:26:08 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.kt
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.kt
@@ -1,10 +1,12 @@
+import kotlin.math.*
+
 class Solution {
     fun solution(a: Int, b: Int): Int {
         var answer: Int = 0
         
         if(a%2 != 0 && b%2 != 0) answer = a*a + b*b
         else if(a%2 != 0 || b%2 != 0) answer = 2*(a+b)
-        else answer = if(a-b < 0) (-1)*(a-b) else a-b
+        else answer = abs(a-b)
 
         return answer
     }
