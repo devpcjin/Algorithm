355bf6b3abe07176345b0d71f33bcd406fddc47d
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-04 09:23:16 +0900

    [level 0] Title: 글자 지우기, Time: 21.13 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.kt
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(my_string: String, indices: IntArray): String {
         
-        return my_string.mapIndexed { i,v -> if(i in indices) "" else v}.joinToString("")
+        return my_string.filterIndexed { i, _ -> i !in indices }
     }
 }
