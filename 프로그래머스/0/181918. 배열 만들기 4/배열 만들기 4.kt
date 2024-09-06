250ec9772095b39b9af15a4b01856cd837a9fe50
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-09-06 09:41:32 +0900

    [level 0] Title: 배열 만들기 4, Time: 88.40 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        var stk: IntArray = intArrayOf()
+        var index: Int = 0;
+        
+        while(index != arr.size) {
+            
+            if(stk.isEmpty() || stk.last() < arr[index]) {
+                stk += arr[index]
+                index++;
+            }
+            else {
+                stk = stk.dropLast(1).toIntArray()
+            }
+        }
+        
+        return stk
+    }
+}
