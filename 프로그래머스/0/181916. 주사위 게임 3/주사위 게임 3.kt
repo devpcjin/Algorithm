791dab7487dec2239170d9d1df977834f7eed101
Cheoljin Park <103913683+devpcjin@users.noreply.github.com>
2024-12-17 14:14:08 +0900

    [level 0] Title: 주사위 게임 3, Time: 25.26 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.kt
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.kt
@@ -7,35 +7,21 @@ class Solution {
         val resultList: List<Int> = listOf(a,b,c,d)
         val resultMap: Map<Int, Int> = resultList.groupingBy {it}.eachCount()
         
-        var p:Int = 0
-        var q:Int = 0
-        var r:Int = 0
-        
-        var checker:Boolean = false
-        
-        when(resultMap.size) {
-            1 -> answer = 1111 * a
+        return when(resultMap.size) {
+            1 ->  1111 * a
             2 -> {
-                resultMap.forEach {
-                    if(it.value == 3) { 
-                        checker = true
-                        p = it.key
-                    }
-                    else if(it.value == 2 && p == 0) p = it.key
-                    else if(it.value == 2 && q == 0) q = it.key
-                    else if(it.value ==1) q = it.key
+                val (p, q) = resultMap.entries.sortedByDescending { it.value }.map { it.key }
+                if(resultMap[p] == 3) {
+                    (10*p+q)*(10*p+q)
+                } else {
+                    (p+q)*abs(p-q)
                 }
-                answer = if(checker) (10*p+q)*(10*p+q) else (p+q)*abs(p-q)
             }
             3 -> {
-                resultMap.forEach {
-                    if(it.value == 2) p = it.key
-                    else if(q == 0) q = it.key
-                    else r = it.key
-                }
-                answer = q*r
+                val (p, q, r) = resultMap.entries.sortedByDescending { it.value }.map { it.key }
+                q*r
             }
-            else -> answer = resultList.minOrNull() ?: 0
+            else -> resultList.minOrNull() ?: 0
         }
         
         return answer
